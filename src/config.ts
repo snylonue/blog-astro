719021f5a4de611223df957a6405eb0060843e96
snylonue <snylonue@gmail.com>
2024-06-23 15:47:32 +0800

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,33 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  author: "snylonue",
+  desc: "snylonue's blog",
+  title: "snylonue's Blog",
+  // ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerPage: 3,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+};
+
+export const LOCALE = {
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+} as const;
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/snylonue",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+];
